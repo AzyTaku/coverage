638babaa5b7e6e8f6a124ded14e248064b35671e
AzyTaku <azlanshaveenraban1@gmail.com>
2025-07-05 09:02:42 +0530

    basic frontend and backend setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// backend/index.js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
